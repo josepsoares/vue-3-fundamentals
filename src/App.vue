f40ddcb888b2be47ac748103155e6c0a04b28f59
José Soares <josepsoares@ua.pt>
2023-03-19 22:43:14 +0000

    finish lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const header = ref('shopping list app')
+const items = ref([
+  { id: 1, label: "10 bananas" },
+  { id: 2, label: "2 cookie boxes" },
+  { id: 3, label: "20 water bottles" }
+])
+const newItem = ref("")
+const newItemHighPriority = ref(false)
+const iceCreamFlavors = ref([])
+
+// v.model.lazy
+// v.model.number
+// v.model.trim
+// v.model can be used on inputs, radio boxes, selects etc.
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ header }}</h1>
+  <form @submit.prevent="items.push({ id: items.length + 1, label: newItem })">
+    <input v-model.trim="newItem" type="text" placeholder="add an item">
+    <label>
+      <input type="checkbox" v-model="newItemHighPriority">
+      high priority
+    </label>
+    <button class="btn btn-primary">save item</button>
+  </form>
+  <ul>
+    <li v-for="({ id, label }, index) in items" :key="id">
+      {{ label }}
+    </li>
+  </ul>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
